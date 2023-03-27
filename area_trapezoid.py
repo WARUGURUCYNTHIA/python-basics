7de455d1d9cbfa5f04e27fb298943bdbec5e500a
WARUGURUCYNTHIA <warugurucynthia4@gmail.com>
2023-03-27 11:33:40 +0300

    area of Trapezoid

---
+++ area_trapezoid.py
@@ -0,0 +1,6 @@
+b1 = float(input("Enter the length of first base: "))
+b2 = float(input("Enter the length of second base: "))
+h = float(input("Enter the length of height:"))
+
+area = (b1 + b2)*h/2
+print(f"Area of Trapezoid is: {area:.2f}")
