818390f0a7f42af4af0b147c079ec71e25977535
WARUGURUCYNTHIA <warugurucynthia4@gmail.com>
2023-03-28 12:43:51 +0300

    Basics of a calculator

---
+++ basic_calc.py
@@ -0,0 +1,30 @@
+def add(num1,num2):
+    result=num1+num2
+    return result
+
+def sub(num1,num2):
+    result=num1-num2
+    return result
+
+def div(num1,num2):
+    result=num1/num2
+    return result
+
+def mult(num1,num2):
+    result=num1*num2
+    return  result
+
+num1=float(input("Enter the first number: "))
+num2=float(input("Enter the second number: "))
+op = input("Enter the operator: ")
+
+if op=="+":
+    print(f"sum is {add(num1,num2)}")
+elif op=="-":
+    print(f"difference is {sub(num1,num2)}")
+    
+elif op=="/":
+    print("quotient is "+str(div(num1,num2))+".")
+    
+elif op=="*":
+    print(f"the product is {mult(num1,num2)}")
